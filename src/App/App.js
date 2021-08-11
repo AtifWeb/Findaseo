279012e5edbff135d46ed9ee19e07a9e6be628b3
Atif Asim <67152713+AtifWeb@users.noreply.github.com>
2021-08-11 12:28:22 -0700

    Update App.js

--- src/App/App.js
+++ src/App/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/dashhboard/settings" exact>
+          <Route path="/dashboard/settings" exact>
           <Settings/>
           </Route>
           <Route path="/dashhboard/contact" exact>
