255e3f08c902c00deddbfd6e3fe2c6201655cbca
AtifWeb <mytechlife96@gmail.com>
2021-10-03 04:12:59 -0700

    Resolve Navbar desktop bugs

--- src/App/component/Atoms/LiveChat/SeconderButton.jsx
+++ src/App/component/Atoms/LiveChat/SeconderButton.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./SeconderButton.module.css";
 import SenderImage from "../../../../Assets/img/sender.svg";
+import { Link } from "react-router-dom";
 export const SenderButton = ({ text, img = null, to = null }) => {
   return (
     <Link className={styles.SeconderButton} to={to}>
