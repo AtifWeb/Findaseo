64dfa2c833d1e6bab1337bf06b92a71b6a585b41
AtifWeb <mytechlife96@gmail.com>
2021-10-03 11:53:54 -0700

    Resolve Navbar desktop bugs

--- src/App/component/templates/LiveChat/LiveChat.jsx
+++ src/App/component/templates/LiveChat/LiveChat.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { CollapseAbleLiveChat } from "../../organisms/LiveChat/CollapseAbleLiveChat";
 import styles from "./LiveChat.module.css";
-import burger from "../../../../Assets/img/burger.png";
+import burger from "../../../../Assets/img/chat.png";
 import { HandleBotDisplay } from "./events/HandleBotDisplay";
 export const LiveChat = () => {
   return (
@@ -12,8 +12,9 @@ export const LiveChat = () => {
         className={styles.button}
         onClick={HandleBotDisplay}
         onTouchStart={HandleBotDisplay}
+        id="burgerButton"
       >
-        <img src={burger} alt="" />
+        <img src={burger} alt="" className={styles.Img} />
       </button>
     </div>
   );
