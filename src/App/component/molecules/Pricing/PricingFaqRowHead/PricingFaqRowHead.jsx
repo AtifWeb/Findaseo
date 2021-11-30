763d40882057923d0646f9a0591f15fdbbdd1b50
AtifWeb <mytechlife96@gmail.com>
2021-11-29 19:59:23 -0800

    new push

--- src/App/component/molecules/Pricing/PricingFaqRowHead/PricingFaqRowHead.jsx
+++ src/App/component/molecules/Pricing/PricingFaqRowHead/PricingFaqRowHead.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./PricingFaqRowHead.module.css";
-export const PricingFaqRowHead = ({ question }) => {
+export const PricingFaqRowHead = ({ question, HandleShow }) => {
   return (
-    <div className={styles.head}>
+    <div className={styles.head} onClick={HandleShow}>
       <h3 className={styles.question}>{question}</h3>
       <div className={styles.dropdown}>
         <i class="fas fa-caret-down"></i>
